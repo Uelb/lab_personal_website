d2c6feeb87fa4205c9d3cb560a8f212bf03c93d7
Thomas THIMOTHEE <thomas@oxynum.fr>
2013-07-16 17:04:42 +0900

    change the website to have  usable template

--- js/global.js
+++ js/global.js
@@ -9,21 +9,11 @@ jQuery(function($){
 		// Components							
 		slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
 		slides 					:  	[			// Slideshow Images
-								{image : 'img/IMG_1438.JPG', title: "Kinkakuji during the momiji season, Kyoto"},
-								{image : 'img/IMG_4267.JPG', title: "Sunset near Osaka castle during ume season, Osaka"},
-								{image : 'img/IMG_5337.JPG', title: "Holy Ise Taisha shrine, Ise"},
-								{image : 'img/IMG_5338.JPG', title: "World Heritage pilgrimage road, Kumano Kodo"},
-								{image : 'img/IMG_5339.JPG', title: "One of the 3 best views of Japan, Miyajima"},
-								{image : 'img/IMG_5341.JPG', title: "Yuki matsuri, Sapporo"},
-								{image : 'img/IMG_5880.JPG', title: "Sakura blooming in romantic Naka-Meguro, Tokyo"},
-								{image : 'img/IMG_6110.JPG', title: "Magical sky during sakura season, Fukuoka"},
-								{image : 'img/IMG_6558.JPG', title: "Charming Nagasaki, Nagasaki"},
-								{image : 'img/IMG_7483.JPG', title: "Okinawa"},
-								{image : 'img/IMG_7591.JPG', title: "Kourijima, Okinawa"},
-								{image : 'img/IMG_7680.JPG', title: "Beautiful sunset in tropical island, Okinawa"},
-								{image : 'img/IMG_8002.JPG', title: "Japan most popular treasure Fujisan, Hakone"},
-								{image : 'img/IMG_8167.JPG', title: "Shibazakura festival, Kawaguchiko"},
-								{image : 'img/IMG_9875.JPG', title: "Ajisai paradise, Shimoda"}
+								{image : 'img/wallpaper_0.jpg', title: ""},
+								{image : 'img/wallpaper_1.jpg', title: ""},
+								{image : 'img/wallpaper_2.jpg', title: ""},
+								{image : 'img/wallpaper_3.jpg', title: ""},
+								{image : 'img/wallpaper_4.jpg', title: ""}
 									],
 		fit_always: 1
 	});
